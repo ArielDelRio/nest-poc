a2d4b9f72c432a6abd5f2c42ee95315be62d296d
ArieldRio <arieldelrioviamonte@gmail.com>
2023-04-28 00:25:11 -0400

    Add hbs as a view engine to test twilio js sdk

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -12,6 +14,10 @@ async function bootstrap() {
   );
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
 
   await app.listen(port || 3000);
 }
